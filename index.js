5b1fde0a509b2133a91700561496e918c79a5778
Chisato8120-netizen <chisato.8120@gmail.com>
2026-02-20 01:03:06 +0900

    セクション３の微調整

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let animation = document.getElementById("animation")
 window.addEventListener("scroll", (e) => {
     console.log(window.scrollY)
-    if (window.scrollY > 1670) {
+    if (window.scrollY > 1000) {
         animation.classList.add("inited")
     }
 })
